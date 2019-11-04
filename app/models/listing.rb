ae04666e4f7056968ff5b2541c5f6dcb93b0bc24
Molly Madden <molly.annette.madden@gmail.com>
2019-11-04 21:32:10 +1100

    fixed precision issue

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -7,11 +7,6 @@ class Listing < ApplicationRecord
   enum gender: { Female: 0, Male: 1 }
   enum drop_off: { Yes: 0, No: 1 }
 
-  def self.search(search)
-    skills_listing = Listing.connection.select_all( "SELECT skills.title, listings.* FROM skills INNER JOIN listings ON listings.skill_id = skills.id WHERE skills.id = #{search}" )
 
-    #Listing.find_by_sql( "SELECT skills.title, listings.* FROM listings INNER JOIN skills ON listings.skill_id = skills.id WHERE #{search}" )
-
-  end
 
 end
